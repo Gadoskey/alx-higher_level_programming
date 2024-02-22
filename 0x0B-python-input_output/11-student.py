93397186b8924875b729d511fd621193a889eb3f
Opeyemi <mustopha.yusufope@gmail.com>
2024-02-22 04:54:23 -0800

    11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+# Author -- Gadoskey
 """This module defines a class Student"""
 
 
 class Student:
-    """Represent a student."""
+    """A class student."""
 
     def __init__(self, first_name, last_name, age):
         """Initializes a new Student
@@ -13,17 +14,15 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """Gets a dictionary representation of the Student.
+        """Get a dictionary representation of the Student
         If attrs is a list of strings, represents only those attributes
         included in the list
         """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
-
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs
+                    if hasattr(self, attr)}
     def reload_from_json(self, json):
-        """Replaces all attributes of the Student
-        """
-        for k, v in json.items():
-            setattr(self, k, v)
+        for key, value in json.items():
+            setattr(self, key, value)
