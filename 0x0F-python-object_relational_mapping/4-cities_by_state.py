c0a475ccfa15e0ad8fa94199fb73ab27d35b5b94
Your Name <you@example.com>
2024-05-22 09:50:22 -0700

    new

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -16,22 +16,18 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
 
-    with db.cursor() as cur:
-        cur.execute("""
-            SELECT
-                cities.id, cities.name, states.name
-            FROM
-                cities
-            JOIN
-                states
-            ON
-                cities.state_id = states.id
-            ORDER BY
-                cities.id ASC
-        """)
+    cursor = db.cursor()
+    query = """
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        ORDER BY cities.id ASC
+    """
+    cursor.execute(query)
+    rows = cursor.fetchall()
 
-        rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
-    if rows is not None:
-        for row in rows:
-            print(row)
+    cursor.close()
+    db.close()
